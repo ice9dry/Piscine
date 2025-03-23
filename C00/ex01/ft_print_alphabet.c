d852d39cb382a7b29363648630362911f4e8328b
Mark Koon Hong Lee <marlee@c2r1s13.42singapore.sg>
2025-03-23 16:02:24 +0800

    Added C00/

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marlee <marlee@student.42student.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 20:56:10 by marlee            #+#    #+#             */
+/*   Updated: 2025/03/20 22:22:45 by marlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		write(1, &alphabet, 1);
+		alphabet++;
+	}
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marlee <marlee@student.42student.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 21:14:10 by marlee            #+#    #+#             */
+/*   Updated: 2025/03/20 22:23:07 by marlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		write(1, &alphabet, 1);
+		alphabet--;
+	}
+}
